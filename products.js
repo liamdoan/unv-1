aa38e32c4f0978816eaaf796e81ce06c828c930a
liamdoan <lamdoan9898@gmail.com>
2025-03-12 01:19:58 +0200

    Updates

---
+++ products.js
@@ -0,0 +1,35 @@
+const products = [
+    {
+        id: 1,
+        title: "Polo Shirt",
+        description: "Lorem 60 ipsum dolor sit amet consectetur adipisicing elit. Vitae veniam fugiat similique aliquid enim ex labore reiciendis quos, voluptate harum vel adipisci, nobis possimus dicta est placeat minus repellendus, maxime autem incidunt odit molestias ad accusantium.",
+        price: "$29.99",
+        photo: "https://img.freepik.com/premium-photo/blank-white-polo-shirt-front-view-isolated-white-background_373676-1552.jpg",
+        details: "Experience our premium Polo Shirt, crafted with organic cotton and featuring pure white color. Perfect for summer enthusiasts!"
+    },
+    {
+        id: 2,
+        title: "Star Hoodie",
+        description: "Lorem 40 ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, voluptates nam culpa incidunt dicta perspiciatis id necessitatibus voluptate repellat iusto asperiores harum?",
+        price: "$49.99",
+        photo: "https://stardustny.com/cdn/shop/products/46864A5D89F7FF9F04AC2C93AD3D26D9_1024x.jpg?v=1573180588",
+        details: "Stay cozy with our Star Hoodie, featuring a unique constellation pattern and comfortable fleece lining. Perfect for stargazing nights!"
+    },
+    {
+        id: 3,
+        title: "Beach  Shorts",
+        description: "Lorem 20 ipsum dolor sit amet consectetur adipisicing elit. Ducimus accusantium esse obcaecati iure vel facere voluptatum molestias.",
+        price: "$34.99",
+        photo: "https://thumbs.dreamstime.com/b/red-swimming-shorts-isolated-white-backgroundred-swimming-shorts-isolated-white-background-red-swimming-shorts-isolated-353885993.jpg",
+        details: "Our Beach Shorts combine style and comfort with a unique stripe phase pattern. Made with quick-dry technology for active lifestyles."
+    },
+    {
+        id: 4,
+        title: "Winter Jacket",
+        description: "Lorem 35 ipsum dolor sit amet consectetur adipisicing elit. Consequatur rem sint repellat, vitae aspernatur voluptate repudiandae labore minima repellendus qui eaque iste dolores.",
+        price: "$79.99",
+        photo: "https://thumbs.dreamstime.com/b/mockup-blank-white-down-jacket-stock-clothing-apparel-347917280.jpg",
+        details: "Make a statement with our water-resistant winter jacket, featuring a royal white theme inspired by the Royal Family."
+    }
+];
+export default products;
